e966f3838e8bd0a927c566263ee885841cb2af91
reeses-pieces <nicholasreis@icloud.com>
2016-12-21 11:42:50 -0800

    add play and skip functionality with event listeners

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,5 +8,36 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 // Build our functions
+function togglePlay() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+
+// Updates the player button depending on video status
+// Alternate way
+function updateButton() {
+  if(video.paused) {
+    toggle.innerHTML = '►';
+  } else {
+    toggle.innerHTML = '❚ ❚';
+  }
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
 
 // Hook up the event listeners
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(btn => btn.addEventListener('click', skip));
