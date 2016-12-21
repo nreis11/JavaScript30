584df9c645ac64cfb66e975f7b3bfe8f1b8d9d41
reeses-pieces <nicholasreis@icloud.com>
2016-12-21 12:04:15 -0800

    add range update function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progresss__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 // Build our functions
 function togglePlay() {
@@ -28,8 +28,13 @@ function updateButton() {
 }
 
 function skip() {
-  console.log(this.dataset.skip);
+  // Convert HTML attr to float
   video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Assign input's name and value to video attr
+function handleRangeUpdate() {
+  video[this.name] = this.value;
 }
 
 // Hook up the event listeners
@@ -39,5 +44,7 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
-
 skipButtons.forEach(btn => btn.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
