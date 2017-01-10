7fbf9f3e20b4d3ec3e84d89711f5d2af5396198c
reeses-pieces <nicholasreis@icloud.com>
2017-01-09 17:06:42 -0800

    add portrait crop coordinates

--- 20 - Speech Detection/beatles/beatles.js
+++ 20 - Speech Detection/beatles/beatles.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  console.log("ran")
-})
+  console.log("ran");
+});
+
+const positions = {
+  "one": {top: 0, right: 385, bottom: 385, left: 0},
+  "two": {top: 0, right: 827, bottom: 385, left: 385},
+  "three": {top: 382, right: 385, bottom: 762, left: 0},
+  "four": {top: 382, right: 800, bottom: 762, left: 385}
+};
+
+// 1st - rect(0px,385px,385px,0px)
+// 2nd - rect(0px,827px,385px,384px)
+// 3rd - rect(382px,385px,762px,0px)
+// 4th - rect(382px,800px,762px,385px)
+
+const changeImgCrop = function(top, right, bottom, left) {
+  $('#portrait').css('clip', `rect(${top}px,${right}px,${bottom}px,${left}px)`);
+};
