86bec89f270ff2760036911ee74c48c11775e918
reeses-pieces <nicholasreis@icloud.com>
2017-02-16 12:07:51 -0800

    add display return time func

--- 29 - Countown Timer/scripts-START.js
+++ 29 - Countown Timer/scripts-START.js
@@ -1,11 +1,12 @@
 let countdown;
 const timeLeft = document.querySelector(".display__time-left");
-
+const endTime = document.querySelector(".display__end-time");
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + (seconds * 1000);
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -19,10 +20,19 @@ function timer(seconds) {
 
 function displayTimeLeft(seconds) {
   const minutesLeft = Math.floor(seconds / 60);
-  console.log('minutesLeft', minutesLeft);
   let secondsLeft = seconds % 60;
   secondsLeft = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
-  console.log('secondsLeft', secondsLeft);
   timeLeft.textContent = `${minutesLeft}:${secondsLeft}`;
   document.title = timeLeft.textContent;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjHour = hour > 12 ? hour - 12 : hour;
+  console.log('hour', hour);
+  let minutes = end.getMinutes();
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  console.log('minutes', minutes);
+  endTime.textContent = `Return at ${adjHour}:${minutes}`;
 }
